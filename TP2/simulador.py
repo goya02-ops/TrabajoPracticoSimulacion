8ba082bc8b0a6f443565ab98d9639658ae2c3e4e
Esteban Karlen <estebankarlen@MacBook-Air-de-Esteban.local>
2026-05-13 17:03:30 -0300

    Arreglo de args

--- TP2/simulador.py
+++ TP2/simulador.py
@@ -12,17 +12,24 @@ def parse_arguments():
         description='Simulación de Ruleta Europea')
 
     parser.add_argument('-c', '--tiradas', type=int, default=10000,
-                        help='Cantidad de tiradas por corrida')
-    parser.add_argument('-n', '--corridas', type=int, default=200,
-                        help='Cantidad de corridas para la distribución de medias')
-    parser.add_argument('-e', '--elegido', type=int, default=18,
-                        help='Número elegido para análisis')
-    parser.add_argument('-s', '--estrategia', type=int, default=18,
+                        help='Cantidad de tiradas')
+    parser.add_argument('-s', '--estrategia', type=str, default='i',
                         help='Estrategia elegida para análisis')
-    parser.add_argument('-a', '--capital', type=int, default=18,
+    parser.add_argument('-a', '--capital', type=str, default='f',
                         help='"i": infinito "f" finito')
+    parser.add_argument('-i', '--apuesta', type=int, default=18,
+                        help='')
     
     return parser.parse_args()
+
+args = parse_arguments()
+cantidad_tiradas = args.tiradas
+if args.capital == 'i':
+    capital_ini = float('inf')
+else:
+    capital_ini = int(args.capital)
+estrategia= args.estrategia
+apuesta = args.apuesta
 
 nro_negros = {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36}
 nro_rojos = {2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35}
@@ -231,26 +238,24 @@ def graficar_evolucion_apuestas(num_tiradas, historial_apuestas, estrategia):
 
 def main():
     # 1. Obtener parámetros del usuario
-    args = parse_arguments()
-    cantidad_tiradas = args.tiradas
-    numero_elegido = args.elegido
-    num_corridas = args.corridas
-    capital_ini= args.capital
-    estrategia= args.estrategia
+
 
 
     print(f'Capital: {args.capital}')
     print(f'Estrategia: {args.estrategia}')
     print(f'Cantidad de tiradas: {args.tiradas}')
-    print(f'Número elegido: {args.elegido}')
-    print(f'Cantidad de corridas: {args.corridas}')
     
     
 if __name__ == "__main__":
     main()
-    estrategia_fibonacci(10000,20,nro_rojos,500)
-    estrategia_dAlembert(10000,20,nro_rojos,500)
-    estrategia_martingala(10000,20,nro_rojos,500)
-    estrategia_paroli(10000,20,nro_rojos,500)
+if args.estrategia == 'i':
+    estrategia_fibonacci(capital_ini,apuesta,nro_rojos,cantidad_tiradas)
+if args.estrategia == 'd':
+    estrategia_dAlembert(capital_ini,apuesta,nro_rojos,cantidad_tiradas)
+if args.estrategia == 'm':
+    estrategia_martingala(capital_ini,apuesta,nro_rojos,cantidad_tiradas)
+if args.estrategia == 'p':
+    estrategia_paroli(capital_ini,apuesta,nro_rojos,cantidad_tiradas)
+ 
 
     
